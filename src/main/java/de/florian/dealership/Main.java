d33574be59ec1a1fad38de9116b7559a41e24b85
floriiian <florianswzz@gmail.com>
2024-06-27 00:02:56 +0200

    First Commit

--- src/main/java/de/florian/dealership/Main.java
+++ src/main/java/de/florian/dealership/Main.java
@@ -4,6 +4,8 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 import java.io.IOException;
 
@@ -13,7 +15,13 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         Document doc = Jsoup.connect("https://www.donedeal.ie/cars?make=Audi").get();
-        String title = doc.title();
-        LOGGER.debug(title);
+        Elements title = doc.getAllElements();
+        for (Element element : title) {
+            if(element.hasClass("Pricestyled__Text-sc-1dt81j8-5 hywplu")) {
+                if(!element.text().contains("p/m")){
+                    LOGGER.info(element.text());
+                }
+            }
+        }
     }
 }
