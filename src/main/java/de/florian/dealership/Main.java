449d1a619bf5f06496c1db9dab59fdb9763543fb
floriiian <florianswzz@gmail.com>
2024-06-27 01:34:37 +0200

    Added Database support and automatic car price selection, database needs a fix.

--- src/main/java/de/florian/dealership/Main.java
+++ src/main/java/de/florian/dealership/Main.java
@@ -2,6 +2,7 @@ package de.florian.dealership;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
